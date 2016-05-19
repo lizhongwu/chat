dfe96f83900ce5b5d27828378c64a366b89608f7
lizhongwu <1289205959@qq.com>
2016-05-19 14:30:40 +0800

    初次提交

--- js/app.js
+++ js/app.js
@@ -7,20 +7,18 @@ angular.module('mod').controller('ctrl', function ($scope, $firebaseArray) {
     $scope.contents = contactBase;
 
     //初始化
-    $scope.init = function () {
-        //昵称
-        var lsNikeName = localStorage.getItem("nikeName");
-        $scope.nikeName = lsNikeName === "" ? "游客" + Math.floor(Math.random() * 100000) : lsNikeName;
-        //性别
-        var lsSex = localStorage.getItem("sex");
-        $scope.sex = lsSex === "" ? "1" : lsSex;
-        $scope.sexMark = $scope.sex === "0" ? "♀" : "♂";
-        //头像
-        //$scope.head = $scope.sex === "1" ? "img/boy-head.jpg?"+Math.random() : "img/girl-head.jpg?"+Math.random();
-        var lsHead = localStorage.getItem("head");
-        $scope.head = lsHead === "" ? "img/h1.jpg?" + Math.random() : lsHead + "?" + Math.random();
-    };
-    $scope.init();
+    //昵称
+    $scope.nikeName = localStorage.getItem("nikeName");
+    console.log($scope.nikeName);
+    $scope.nikeName = $scope.nikeName === null ? "游客" + Math.floor(Math.random() * 100000) : $scope.nikeName;
+    //性别
+    $scope.sex = localStorage.getItem("sex");
+    $scope.sex = $scope.sex === null ? "1" : $scope.sex;
+    $scope.sexMark = $scope.sex === "0" ? "♀" : "♂";
+    //头像
+    //$scope.head = $scope.sex === "1" ? "img/boy-head.jpg?"+Math.random() : "img/girl-head.jpg?"+Math.random();
+    $scope.head = localStorage.getItem("head");
+    $scope.head = $scope.head === null ? "img/h1.jpg?" + Math.random() : $scope.head + "?" + Math.random();
 
     //发送消息
     $scope.send = function () {
